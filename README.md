6211c27ea18f39795c14009aa295674f03408e2f
kxrxll <kxrxll@kxrxlls-MacBook-Air.local>
2021-11-14 22:42:15 +0300

    done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Drag'n'drop [![Build status](https://ci.appveyor.com/api/projects/status/nlyabcpcb63rhphk?svg=true)](https://ci.appveyor.com/project/kxrxll/ahj-anim)
+# Drag'n'drop [![Build status](https://ci.appveyor.com/api/projects/status/whk49f46dmo72f77?svg=true)](https://ci.appveyor.com/project/kxrxll/ahj-media)
 
-Pages link: https://kxrxll.github.io/ahj-anim/
+Pages link: https://kxrxll.github.io/ahj-media/
--- src/js/Timeline.js
+++ src/js/Timeline.js
@@ -3,6 +3,8 @@ export default class Timeline {
     this.el = el;
     this.form = this.el.querySelector('.timelineForm');
     this.block = this.el.querySelector('.timelineBlock');
+    this.popup = this.el.querySelector('.timelinePop');
+    this.popupForm = this.el.querySelector('.timelinePopForm');
   }
 
   init() {
@@ -11,21 +13,36 @@ export default class Timeline {
       const date = new Date();
       const input = evt.target.querySelector('.timelineInput');
       const text = input.value;
-      let geo = 'unknown';
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          geo = `[${position.coords.latitude}, ${position.coords.longitude}]`;
-        });
+        navigator.geolocation.getCurrentPosition(this.getLocation.bind(this));
+      }
+      if (this.latitude === undefined && this.longitude === undefined) {
+        this.popup.classList.remove('hide');
+        this.popupForm.addEventListener('submit', this.setLocation.bind(this));
       }
       const newPost = document.createElement('div');
       newPost.classList.add('timelinePost');
       newPost.innerHTML = `
         <p class="timelineDate">${date}</p>
         <p class="timelineText">${text}</p>
-        <p class="timelineGeo">${geo}</p>
+        <p class="timelineGeo">${this.geo}</p>
       `;
       input.value = '';
       this.block.appendChild(newPost);
     });
+  }
+
+  getLocation(position) {
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+    this.geo = `[${this.latitude}, ${this.longitude}]`;
+  }
+
+  setLocation(evt) {
+    evt.preventDefault();
+    const coordsArr = evt.target.querySelector('.timelinePopInput').value.split(' ');
+    [this.latitude, this.longitude] = coordsArr;
+    this.popup.classList.add('hide');
+    this.geo = `[${this.latitude}, ${this.longitude}]`;
   }
 }
