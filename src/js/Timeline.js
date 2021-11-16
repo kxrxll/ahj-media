9aa5cd184fae1a0fa4bd6233ef921b579385dfef
kxrxll <kxrxll@kxrxlls-MacBook-Air.local>
2021-11-17 02:08:07 +0300

    done

--- src/js/Timeline.js
+++ src/js/Timeline.js
@@ -8,41 +8,58 @@ export default class Timeline {
   }
 
   init() {
-    this.form.addEventListener('submit', (evt) => {
+    this.form.addEventListener('submit', async (evt) => {
       evt.preventDefault();
       const date = new Date();
       const input = evt.target.querySelector('.timelineInput');
-      const text = input.value;
+      this.text = input.value;
+      const { text } = this;
+      input.value = '';
+      let geo = '';
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(this.getLocation.bind(this));
-      }
-      if (this.latitude === undefined && this.longitude === undefined) {
+        let position;
+        try {
+          position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+          });
+        } catch (err) {
+          this.popup.classList.remove('hide');
+          this.popupForm.addEventListener('submit', this.setLocation.bind(this));
+        }
+        const { latitude, longitude } = position.coords;
+        geo = `[${latitude}, ${longitude}]`;
+        const newPost = document.createElement('div');
+        newPost.classList.add('timelinePost');
+        newPost.innerHTML = `
+        <p class="timelineDate">${date}</p>
+        <p class="timelineText">${text}</p>
+        <p class="timelineGeo">${geo}</p>
+      `;
+        this.block.appendChild(newPost);
+        this.text = '';
+      } else {
         this.popup.classList.remove('hide');
         this.popupForm.addEventListener('submit', this.setLocation.bind(this));
       }
-      const newPost = document.createElement('div');
-      newPost.classList.add('timelinePost');
-      newPost.innerHTML = `
-        <p class="timelineDate">${date}</p>
-        <p class="timelineText">${text}</p>
-        <p class="timelineGeo">${this.geo}</p>
-      `;
-      input.value = '';
-      this.block.appendChild(newPost);
     });
-  }
-
-  getLocation(position) {
-    this.latitude = position.coords.latitude;
-    this.longitude = position.coords.longitude;
-    this.geo = `[${this.latitude}, ${this.longitude}]`;
   }
 
   setLocation(evt) {
     evt.preventDefault();
+    const date = new Date();
+    const { text } = this;
     const coordsArr = evt.target.querySelector('.timelinePopInput').value.split(' ');
-    [this.latitude, this.longitude] = coordsArr;
+    const [latitude, longitude] = coordsArr;
     this.popup.classList.add('hide');
-    this.geo = `[${this.latitude}, ${this.longitude}]`;
+    const geo = `[${latitude}, ${longitude}]`;
+    const newPost = document.createElement('div');
+    newPost.classList.add('timelinePost');
+    newPost.innerHTML = `
+        <p class="timelineDate">${date}</p>
+        <p class="timelineText">${text}</p>
+        <p class="timelineGeo">${geo}</p>
+      `;
+    this.block.appendChild(newPost);
+    this.text = '';
   }
 }
